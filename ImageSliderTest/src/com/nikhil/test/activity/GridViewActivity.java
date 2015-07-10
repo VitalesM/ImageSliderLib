4ae48d3adb0fbf579ea0ec3e3f58c19966161dac
NikhilVijayakumar <nikhil.vijaykumar@gmail.com>
2015-07-10 16:51:50 +0530

    adding menu

--- ImageSliderTest/src/com/nikhil/test/activity/GridViewActivity.java
+++ ImageSliderTest/src/com/nikhil/test/activity/GridViewActivity.java
@@ -6,6 +6,8 @@ import android.app.Activity;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.util.TypedValue;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.GridView;
 
 import com.nikhil.imagesliderlib.adapter.GridViewImageAdapter;
@@ -20,7 +22,6 @@ public class GridViewActivity extends Activity {
 	private GridViewImageAdapter adapter;
 	private GridView gridView;
 	private int columnWidth;
-	
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -30,35 +31,23 @@ public class GridViewActivity extends Activity {
 		gridView = (GridView) findViewById(R.id.grid_view);
 
 		utils = new Utils(GridViewActivity.this);
-		
-		
-		AppConstant.gridviewClickListener = new GridImageViewListner(0,this);
+
+		AppConstant.gridviewClickListener = new GridImageViewListner(0, this);
 
 		// Initilizing Grid View
 		InitilizeGridLayout();
 
 		// loading all image paths from SD card
-		
-		int[] drawable = {R.drawable.ic_01,
-				R.drawable.ic_02,
-				R.drawable.ic_03,
-				R.drawable.ic_04,
-				R.drawable.ic_05,
-				R.drawable.ic_06,
-				R.drawable.ic_07,
-				R.drawable.ic_08,
-				R.drawable.ic_09,
-				R.drawable.ic_10,
-				R.drawable.ic_11,
-				R.drawable.ic_12,
-				R.drawable.ic_13,
-				R.drawable.ic_14,
-				R.drawable.ic_15,};
+
+		int[] drawable = { R.drawable.ic_01, R.drawable.ic_02,
+				R.drawable.ic_03, R.drawable.ic_04, R.drawable.ic_05,
+				R.drawable.ic_06, R.drawable.ic_07, R.drawable.ic_08,
+				R.drawable.ic_09, R.drawable.ic_10, R.drawable.ic_11,
+				R.drawable.ic_12, R.drawable.ic_13, R.drawable.ic_14,
+				R.drawable.ic_15, };
 		AppConstant.drawablelist = drawable;
-		
-	
-		
-		AppConstant.setBitmapDrawable(GridViewActivity.this,columnWidth);
+
+		AppConstant.setBitmapDrawable(GridViewActivity.this, columnWidth);
 
 		// Gridview adapter
 		adapter = new GridViewImageAdapter(GridViewActivity.this,
@@ -82,6 +71,28 @@ public class GridViewActivity extends Activity {
 				(int) padding);
 		gridView.setHorizontalSpacing((int) padding);
 		gridView.setVerticalSpacing((int) padding);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// toggle nav drawer on selecting action bar app icon/title
+
+		return super.onOptionsItemSelected(item);
+
+	}
+
+	@Override
+	public boolean onPrepareOptionsMenu(Menu menu) {
+		// if nav drawer is opened, hide the action items
+
+		// menu.findItem(R.id.action_settings).setVisible(!drawerOpen);
+		return super.onPrepareOptionsMenu(menu);
 	}
 
 }
